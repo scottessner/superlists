b46a42932ef767ac5f6b1ad544cbfd815cbbdea9
ScottEssner <scott.essner@gmail.com>
2016-11-22 16:41:23 -0600

    Use bootstrap to imporve layout

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -1,4 +1,4 @@
-from django.test import LiveServerTestCase
+from django.contrib.staticfiles.testing import StaticLiveServerTestCase
 from selenium import webdriver
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions
@@ -7,7 +7,7 @@ from selenium.webdriver.common.by import By
 
 import time
 
-class NewVisitorTest(LiveServerTestCase):
+class NewVisitorTest(StaticLiveServerTestCase):
 
     def setUp(self):
         self.browser = webdriver.Firefox()
