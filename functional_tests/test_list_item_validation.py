88fc7eb337eccae12ddf3eb8b2c5c6ac180857e1
ScottEssner <scott.essner@gmail.com>
2016-12-15 15:52:11 -0600

    enforce model validation in list view

--- functional_tests/test_list_item_validation.py
+++ functional_tests/test_list_item_validation.py
@@ -2,6 +2,7 @@ from .base import FunctionalTest
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from unittest import skip
+import time
 
 class ItemValidationTest(FunctionalTest):
 
@@ -9,7 +10,8 @@ class ItemValidationTest(FunctionalTest):
         # Scott goes to the home page and accidentally tries to submit
         # an empty list item. He hits enter on the empty input box
         self.browser.get(self.server_url)
-        self.browser.find_element_by_id('id_new_item').send_keys('\n')
+        inputbox =  self.browser.find_element_by_id('id_new_item')
+        inputbox.send_keys(Keys.ENTER)
 
         # The home page refreshes and there is an error message saying 
         # that list items cannot be blank
@@ -17,18 +19,23 @@ class ItemValidationTest(FunctionalTest):
         self.assertEqual(error.text, "You can't have an empty list item")
 
         # He tries again with some text for the item, which now works.
-        self.browser.find_element_by_id('id_new_item').send_keys('Buy milk\n')
-        self.check_for_row_in_list_table('1. Buy milk')
+        inputbox =  self.browser.find_element_by_id('id_new_item')
+        inputbox.send_keys('Buy milk')
+        inputbox.send_keys(Keys.ENTER)
+        self.check_for_row_in_list_table('1: Buy milk')
 
         # Perversely, he now decides to submit a second blank list item
-        self.browser.find_element_by_id('id_new_item').send_keys('\n')
+        inputbox =  self.browser.find_element_by_id('id_new_item')
+        inputbox.send_keys(Keys.ENTER)
         
         # He receives a similar warning on the list page
-        self.check_for_row_in_list_table('1. Buy milk')
+        self.check_for_row_in_list_table('1: Buy milk')
         error = self.browser.find_element_by_css_selector('.has-error')
         self.assertEqual(error.text, "You can't have an empty list item")
 
         # And he can correct it by filling some text in
-        self.browser.find_element_by_id('id_new_item').send_keys('Make tea\n')
-        self.check_for_row_in_list_table('1. Buy milk')
-        self.check_for_row_in_list_table('2. Make tea')
+        inputbox =  self.browser.find_element_by_id('id_new_item')
+        inputbox.send_keys('Make tea')
+        inputbox.send_keys(Keys.ENTER)
+        self.check_for_row_in_list_table('1: Buy milk')
+        self.check_for_row_in_list_table('2: Make tea')
